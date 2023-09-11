4280f060a12bb052ae35f791146996bc856514c3
Nidash <nidash@gmail.com>
2023-09-11 12:49:37 +0100

    Adding styles for Button and Adding Cart Icon.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
-    </div>
+    </Fragment>
   );
 }
 
