33c4da4fb9409fbe70a901650a65044a33985141
Nidash <nidash@gmail.com>
2023-09-13 17:12:12 +0100

    Using portals and Modal

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -7,10 +7,10 @@ const MealItemForm = () => {
         <Input label="Amount" input={{
             id: 'amount',
             type: 'number',
-            min: 1,
-            max: 5,
-            step: 1,
-            defaultValue: 1
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1'
         }}/>
         <button>+ Add</button>
     </form>
