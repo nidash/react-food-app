31ec3c23e5eff5932092ddef8f6a4b71961dff93
Nidash <nidash@gmail.com>
2023-09-16 14:41:36 +0100

    wip

---
+++ src/store/CartProvider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CartContext from './cart-context';
+const CartProvider = props => {
+    return <CartContext.Provider>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
